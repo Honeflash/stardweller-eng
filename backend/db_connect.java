206cd30785c5e5de78f34e58fc24cb8034849067
Honeflash <camnic4908@gmail.com>
2026-03-06 23:05:16 -0500

    JDBC Testing
    Successful test of JDBC connectivity.

--- backend/db_connect.java
+++ backend/db_connect.java
@@ -1,3 +1,28 @@
+import java.sql.*;
+
 public class db_connect {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5818418";
+        String user = "sql5818418";
+        String password = "N6hXtIzRrG";
+        String query = "SELECT * FROM Words";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url, user, password);
+        System.out.println("Connection Successful.");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(query);
+
+        while (rs.next()) {
+            String text = rs.getString("Stardweller");
+            System.out.println(text);
+        }
+        
+        con.close();
+        st.close();   
+        System.out.println("Connection Closed.");
+
+    }
     
 }
