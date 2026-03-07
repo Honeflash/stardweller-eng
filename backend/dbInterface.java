640d031c9b92787ed17aa2be8b5ea3b027226dcd
Honeflash <camnic4908@gmail.com>
2026-03-07 02:20:28 -0500

    Offline Database Setup
    Code created to allow the database to be viewed and have values inserted offline for convenience sake.

---
+++ backend/dbInterface.java
@@ -0,0 +1,94 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class dbInterface {
+
+    static final String URL = "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5818418";
+    static final String USERNAME = "sql5818418";
+    static final String PASSWORD = "N6hXtIzRrG";
+    static final String MAIN_MENU = "Select an option from the list of available service in this interface:\n"
+    + "1) View the entire table\n2) Add to the table\n3) Close connection & Log off\n" +"H) Help\n\nYour Choice: ";
+    static final String GET_ALL_QUERY = "SELECT * FROM Words ORDER BY English ASC";
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        // Database variables
+        Connection dbConnection;
+        Statement dbStatement;
+
+        // User input variables
+        @SuppressWarnings("resource")
+        Scanner scn = new Scanner(System.in);
+        char menuInput = ' ';
+        char insInput = ' ';
+
+        //Other variables
+        boolean isInserting = false; // Handles the while loop for inserting into the database's table
+        String value1 = "", value2 = ""; // Values for inserting into the database's table
+        
+        // Initial connection to the database 
+        dbConnection = db_connect.connectToDatabase(URL, USERNAME, PASSWORD);
+        if (dbConnection.isValid(0)) {
+            System.out.println("Connection Established.\nEntering the Database Remote Interface...\n\n");
+        }
+        dbStatement = dbConnection.createStatement();
+
+        // While loop for continuing until either the connection is interrupted or until the user manually ends the session
+        while (dbConnection.isValid(0)) {
+            System.out.print(MAIN_MENU);
+            menuInput = scn.next().charAt(0); // Get input from the user over their next option
+            scn.nextLine(); // This, and all random placements of scn.nextLine() are here to ensure the newline character is properly processed
+
+            if (menuInput == '3') {
+                System.out.println("Logging out, please wait...");
+                return;
+            }
+
+            // Handle processing the user's choice
+            switch (menuInput) {
+                case '1':
+                    db_connect.viewAll(dbStatement, GET_ALL_QUERY); // Use the previous defind query to return all values in the table
+                    break;
+                case '2':
+                    isInserting = true; // Enter the while loop for inserting into the table
+
+                    while (isInserting) {
+                        // Get the user's input
+                        System.out.print("Insert the word to add in the English column: ");
+                        value1 = scn.nextLine();
+                        System.out.print("Insert the word to add in the Stardweller column: ");
+                        value2 = scn.nextLine();
+
+                        db_connect.insert(dbConnection, value1, value2); // Call the insert function
+
+                        // Confirm if the user would like to continue or not
+                        System.out.print("Add another set of values?\nResponse (Y/N): ");
+                        insInput = scn.next().charAt(0);
+
+                        // Handle invalid user input
+                        while (insInput != 'Y' && insInput != 'y' && insInput != 'N' && insInput != 'n'){
+                            System.out.println("You have entered an invalid option. Please, enter a valid option.\n");
+                            System.out.print("Add another set of values?\nResponse (Y/N): ");
+                            insInput = scn.next().charAt(0);
+                        }
+                        scn.nextLine();
+                        
+                        if (insInput == 'N' || insInput == 'n') { isInserting = false; } // Exit the loop if the user chooses to
+                    }
+
+                    break;
+                case 'H', 'h':
+                    // Placeholder
+                    break;
+                default:
+                    System.out.println("You have entered an invalid option. Please, enter a valid option.\n");
+                    break;
+            }
+        }
+
+        // Close all connections, both database and user input related
+        dbConnection.close();
+        dbStatement.close();
+        scn.close();
+    }
+
+}
--- backend/db_connect.java
+++ backend/db_connect.java
@@ -2,27 +2,57 @@ import java.sql.*;
 
 public class db_connect {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        String url = "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5818418";
-        String user = "sql5818418";
-        String password = "N6hXtIzRrG";
-        String query = "SELECT * FROM Words";
+    static final String INSERT_INTO_QUERY = "INSERT INTO Words (English, Stardweller) VALUES (?, ?)";
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, user, password);
-        System.out.println("Connection Successful.");
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(query);
+    /**
+     * Allows the specified connection variable to be used to connect to a database as specified by the provided url, username, and password
+     * @param url - The link to the webpage in the format jdbc.mysql://host:port/name
+     * @param user - The username associated with the database
+     * @param pwd - The password associated with the database
+     * @return Returns the attempt to connect to the database
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    static Connection connectToDatabase(String url, String user, String pwd ) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver"); 
+        return DriverManager.getConnection(url, user, pwd); // Connection is attempted here
+    }
 
-        while (rs.next()) {
-            String text = rs.getString("Stardweller");
-            System.out.println(text);
+    /**
+     * Returns the query of the database's entire library in ascending order
+     * @param st - Statement variable used to execute the database query
+     * @param query - The query as a string
+     * @throws SQLException
+     */
+    static void viewAll(Statement st, String query)  throws SQLException {
+        ResultSet rs = st.executeQuery(query); // Execute the query
+
+        // Format and print out the results
+        System.out.println("\tEnglish\t\t\tStardweller");
+        System.out.println("---------------------------------------------------");
+        while (rs.next()){
+            String engWord = rs.getString("English");
+            String strWord = rs.getString("Stardweller");
+            System.out.println("\t" + engWord + "\t\t\t" + strWord);
         }
-        
-        con.close();
-        st.close();   
-        System.out.println("Connection Closed.");
+    }
 
+    /**
+     * Allows for the insertion of values into the specified database
+     * @param c - A representation of the connection to the database
+     * @param v1 - The value for column 1
+     * @param v2 - The value for column 2
+     * @throws SQLException
+     */
+    static void insert(Connection c, String v1, String v2) throws SQLException {
+        // The static String above is used as a template for ensuring that the values are entered properly
+        PreparedStatement p = c.prepareStatement(INSERT_INTO_QUERY);
+        p.setString(1, v1);
+        p.setString(2, v2);
+        int affectedRows = p.executeUpdate(); // Perform the insertion
+
+        // Report back the successful insertion
+        System.out.println(affectedRows + " row(s) inserted successfully\n");
     }
     
 }
