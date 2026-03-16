60c0d1b2a4b8463ebb75787ba16b00a625908194
Honeflash <camnic4908@gmail.com>
2026-03-16 04:29:36 -0400

    Frontend work (#6)
    * Frontend Skeleton
    
    Updated the html file and css files with proper information and styling.
     - html front page is complete. Important areas are given IDs.
     - stylesheet applies formatting to each element.
    
    Added main.js for future development. Currently contains code to add the current year to the footer.
    
    * Webpage Update

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,10 @@
 document.getElementById('year').innerText = new Date().getFullYear();
+
+const copyButton = document.getElementById('copy-text');
+copyButton.addEventListener("click", function toClipboard() {
+    try {
+            navigator.clipboard.writeText(document.getElementById("str-area").innerText);
+        } catch(error){
+            console.error("Failed to copy.", error);
+        }
+});
