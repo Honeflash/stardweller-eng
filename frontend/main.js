4321bcf7635f88778ec669e27790ae0042c41bf2
Honeflash <camnic4908@gmail.com>
2026-04-23 20:10:00 -0400

    Fixing 3 Errors

--- frontend/main.js
+++ frontend/main.js
@@ -23,12 +23,12 @@ copyButton.addEventListener("click", function() {
 
 // -------------------------------------------- Handles using the translate button on the webpage --------------------------------------------
 const translateButton = document.getElementById('send-query');
-const url = "stardweller-eng-production.up.railway.app/translator";
+const url = "https://stardweller-eng-production.up.railway.app";
 
 translateButton.addEventListener("click", function() {              // Basic testing function for managing api calls.
     let baseData = document.getElementById('eng-area').value;       // Gets the current value from the textarea to send in a fetch request
 
-    fetch(`${url}?initString=${encodeURIComponent(baseData)}`)
+    fetch(`${url}/translator?initString=${encodeURIComponent(baseData)}`)
         .then(response => response.text())
         .then(data => {                                             // For testing, data is sent to the console, and displayed in the second textarea
         console.log(data)
